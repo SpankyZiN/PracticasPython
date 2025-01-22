46a11f81a9e4a9330eca9a0db3bdf8eb4389ef2b
Alejandro <alecolman0410@gmail.com>
2025-01-22 17:03:42 -0300

    Ordenacion de archivos y practia de arrays

---
+++ curso_Mouredev/04_list.py
@@ -0,0 +1,23 @@
+### Listas ###
+
+my_list = list()
+my_other_list = []
+
+print(len(my_list))
+
+my_list = [10, 15, 20, 25, 30, 30, 10]
+
+print(my_list)
+print(len(my_list))
+
+my_other_list = [20, 1.81 , "Alejandro", "Colman"]
+
+print(my_other_list)
+print(len(my_other_list))
+
+print(my_other_list[0])
+print(my_other_list[1])
+print(my_other_list[-1])
+print(my_other_list[-3])
+
+print(my_list.count(30))
